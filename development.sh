51c37de0fabd5daf817831a541b2e220635f30ef
meteor95 <mochamad.aries.styawan@gmail.com>
2025-03-11 07:28:39 +0700

    Update Template Latest

--- development.sh
+++ development.sh
@@ -1,2 +1,4 @@
+#/bin/bash
+
 cd source
 php artisan octane:start --port=12101 --watch
--- source/resources/views/paneladmin/content/auth/login.blade.php
+++ source/resources/views/paneladmin/content/auth/login.blade.php
@@ -1,6 +1,48 @@
 @extends('paneladmin.template')
 @section('main_content')
-<div class="text-center">TEST LOGIN</div>
+<div class="container-fluid">
+    <div class="row">
+    <div class="col-xl-5"><img class="bg-img-cover bg-center" src="{{ asset('template/riho/images/login/3.jpg') }}" alt="looginpage"></div>
+    <div class="col-xl-7 p-0">    
+        <div class="login-card login-dark">
+        <div>
+            <div><a class="logo text-start" href="index.html"> <img class="img-fluid for-dark" src="{{ asset('template/riho/images/logo/logo.png') }}" alt="looginpage"><img class="img-fluid for-light" src="{{ asset('template/riho/images/logo/logo_dark.png') }}" alt="looginpage"></a></div>
+            <div class="login-main"> 
+            <form class="theme-form">
+                <h4>Masukan Kredensial ID Kamu</h4>
+                <p>Masukan surel dan katasandi untuk melanjutkan</p>
+                <div class="form-group">
+                <label class="col-form-label">Surel atau Nama Pengguna</label>
+                <input class="form-control" type="email" required="" placeholder="Test@gmail.com">
+                </div>
+                <div class="form-group">
+                <label class="col-form-label">Kata Sandi</label>
+                <div class="form-input position-relative">
+                    <input class="form-control" type="password" name="login[password]" required="" placeholder="*********">
+                    <div class="show-hide"><span class="show">                         </span></div>
+                </div>
+                </div>
+                <div class="form-group mb-0">
+                <div class="checkbox p-0">
+                    <input id="checkbox1" type="checkbox">
+                    <label class="text-muted" for="checkbox1">Simpan untuk perangkat ini</label>
+                </div>
+                <button class="btn btn-primary btn-block w-100" type="submit">Masuk</button>
+                </div>
+                <h6 class="text-muted mt-4 or">Atau Masuk</h6>
+                <div class="social mt-4">
+                <div class="btn-showcase"><a class="btn btn-light" href="https://www.linkedin.com/login" target="_blank"><i class="txt-linkedin" data-feather="linkedin"></i> Link  edIn </a><a class="btn btn-light" href="https://twitter.com/login?lang=en" target="_blank"><i class="txt-twitter" data-feather="twitter"></i>twitter</a><a class="btn btn-light" href="https://www.facebook.com/" target="_blank"><i class="txt-fb" data-feather="facebook"></i>facebook</a></div>
+                </div>
+                <p class="mt-4 mb-0 text-center">Belum Punya Akun?<a class="ms-2" href="sign-up.html">Buat Akun</a></p>
+            </form>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
 @endsection
 @push('scripts')
+<script src="{{ asset('template/riho/js/config.js') }}"></script>
+<script src="{{ asset('template/riho/js/script.js') }}"></script>
 @endpush
